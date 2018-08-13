e318756652269223160b53652c1d73949a3d550a
hamdle <contact@ericmarty.co>
2018-08-12 18:40:24 -0700

    Behavior delegation style example

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-function Foo(who) {
-    this.me = who;
-}
-Foo.prototype.identify = function() {
-    return "I am " + this.me;
+var Foo = {
+    init: function(who) {
+        this.me = who;
+    },
+    identify: function() {
+        return "I am " + this.me;
+    }
 };
 
-function Bar(who) {
-    Foo.call(this, who);
-}
-Bar.prototype = Object.create(Foo.prototype);
+var Bar = Object.create(Foo);
 
-Bar.prototype.speak = function() {
+Bar.speak = function() {
     alert("Hello, " + this.identify());
 };
 
-var b1 = new Bar( "b1" );
-var b2 = new Bar( "b2" );
+var b1 = Object.create( Bar );
+b1.init( "b1" );
+var b2 = Object.create( Bar );
+b2.init( "b2" );
 
 //b1.speak();
 //b2.speak();
